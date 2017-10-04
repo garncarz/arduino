353cda952955cc2df0b96bf59120fd27d2d3f800
Ondřej Garncarz <ondrej@garncarz.cz>
2017-10-04 05:10:27 +0200

    Python script parallelized.

--- LEDs_serial/script.py
+++ LEDs_serial/script.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python3
 
-# TODO use threads, so serial input can be read/printed on the fly
+import multiprocessing
 
 import getch
 import serial
 
-ser = serial.Serial('/dev/ttyS2', 9600)  # Cygwin
-print(ser.read_all().decode().strip())
 
-while True:
+ser = serial.Serial('/dev/ttyS2', 9600)  # Cygwin
+
+
+def check_output():
+    output = ''
+    while True:
+        output += ser.read_all().decode()
+        if '\n' in output:
+            line, output = output.split('\n', 1)
+            print(line)
+
+
+def send_keys():
+    while True:
+        try:
+            key = getch.getch()
+            ser.write([int(key)])
+        except Exception as e:
+            print(e)
+
+
+def main():
+    watcher = multiprocessing.Process(target=check_output)
+    sender = multiprocessing.Process(target=send_keys)
+
     try:
-        key = getch.getch()
-        ser.write([int(key)])
-        print(ser.read_all().decode().strip())
-    except Exception as e:
-        print(e)
+        watcher.start()
+        sender.start()
+
+        watcher.join()
+        sender.join()
+
+    except KeyboardInterrupt:
+        sender.terminate()
+        watcher.terminate()
+
+
+if __name__ == '__main__':
+    main()
