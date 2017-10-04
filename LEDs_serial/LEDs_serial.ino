4df632ad2a29e03121a079f5bb0dd9564b30d9ad
Ondřej Garncarz <ondrej@garncarz.cz>
2017-10-04 12:29:24 +0200

    LED animations for special keys.

--- LEDs_serial/LEDs_serial.ino
+++ LEDs_serial/LEDs_serial.ino
@@ -18,7 +18,9 @@ unsigned long tilt_last_time;
 
 const int LEDS = 6;
 const int LED_OUT[] = {2, 3, 4, 5, 6, 7};
-bool led_value[6];
+bool led_value[LEDS], old_led_value[LEDS];
+
+const int DELAY = 100;
 
 bool DEBUG = 1;
 
@@ -36,7 +38,14 @@ void debug(String msg, bool nl=1, bool force=0) {
 }
 
 
+int mod(int a, int b=LEDS) {
+  return ((a % b) + b) % b;
+}
+
+
 void set_led(int i, bool value) {
+  i = mod(i);
+  
   debug("Setting LED " + String(i) + " to " + String(value));
   led_value[i] = value;
 
@@ -48,10 +57,13 @@ void set_led(int i, bool value) {
 }
 
 
+bool get_led(int i) {
+  return led_value[mod(i)];
+}
+
+
 void toggle_led(int i) {
-  if (i >= 0 && i < LEDS) {
-    set_led(i, !led_value[i]);
-  }
+  set_led(i, !get_led(i));
 }
 
 
@@ -98,5 +110,136 @@ void loop() {
 
 void serialEvent() {
   int in = Serial.read();
-  toggle_led(in);
+  switch(in) {
+    case 20: shift_up(); break;  // left, up
+    case 21: shift_down(); break;  // right, down
+    case 22: blink_off(); break;  // space
+    case 23: blink_off_on(); break;  // enter
+    case 24: backspace(); break;  // backspace
+    case 25: del(); break;  // delete
+    case 26: pg_up(); break;  // page up
+    case 27: pg_down(); break;  // page down
+    case 28: blink_one(0); break;  // home
+    case 29: blink_one(-1); break;  // end
+    default: toggle_led(in);
+  }
+}
+
+
+bool get_old_led(int i) {
+  return old_led_value[mod(i)];
+}
+
+
+void backup_leds() {
+  for (int i = 0; i < LEDS; i++) {
+    old_led_value[i] = led_value[i];
+  }
+}
+
+
+void restore_leds() {
+  for (int i = 0; i < LEDS; i++) {
+    set_led(i, get_old_led(i));
+  }
+}
+
+
+void shift_up() {
+  backup_leds();
+  for (int i = 1; i <= LEDS; i++) {
+    set_led(i, get_old_led(i - 1));
+  }
+}
+
+void shift_down() {
+  backup_leds();
+  for (int i = LEDS - 2; i >= -1; i--) {
+    set_led(i, get_old_led(i + 1));
+  }
+}
+
+
+void blink_off() {
+  backup_leds();
+  
+  for (int i = 0; i < LEDS; i++) {
+    set_led(i, 0);
+  }
+  delay(DELAY);
+  
+  for (int i = 0; i < LEDS; i++) {
+    set_led(i, get_old_led(i));
+  }
+}
+
+
+void blink_off_on() {
+  backup_leds();
+  
+  for (int i = 0; i < LEDS; i++) {
+    set_led(i, 0);
+  }
+  delay(DELAY / 2);
+  
+  for (int i = 0; i < LEDS; i++) {
+    set_led(i, 1);
+  }
+  delay(DELAY / 2);
+  
+  for (int i = 0; i < LEDS; i++) {
+    set_led(i, get_old_led(i));
+  }
+}
+
+
+void backspace() {
+  for (int i = LEDS - 1; i >= 0; i--) {
+    if (get_led(i)) {
+      set_led(i, 0);
+      break;
+    }
+  }
+}
+
+
+void del() {
+  for (int i = 0; i < LEDS - 1; i++) {
+    set_led(i, get_led(i + 1));
+  }
+  set_led(LEDS - 1, 0);
+}
+
+
+void pg_up() {
+  for (int i = 0; i < LEDS; i++) {
+    shift_up();
+    delay(DELAY / LEDS);
+  }
+}
+
+
+void pg_down() {
+  for (int i = 0; i < LEDS; i++) {
+    shift_down();
+    delay(DELAY / LEDS);
+  }
+}
+
+
+void blink_one(int i) {
+  int old = get_led(i);
+
+  set_led(i, 1);
+  delay(DELAY / 5);
+  set_led(i, 0);
+  delay(DELAY / 5);
+  set_led(i, 1);
+  delay(DELAY / 5);
+  set_led(i, 0);
+  delay(DELAY / 5);
+  set_led(i, 1);
+  delay(DELAY / 5);
+  
+  set_led(i, old);
 }
--- LEDs_serial/all_keys.py
+++ LEDs_serial/all_keys.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 
+import re
+
 import keyboard
 import serial
 
-LEDS = 6
 
 ser = serial.Serial('COM3', 9600)  # Windows
+
+
+with open('LEDs_serial.ino') as f:
+    INO_LINES = f.read()
+
+
+LEDS = int(re.search(r'const int LEDS = (\d+);', INO_LINES).group(1))
+
+
+def get_ino_value(name, default):
+    if len(name) > 1:
+        m = re.search(r'case (\d+): .* //.*%s' % name, INO_LINES)
+        if m:
+            return int(m.group(1))
+    return default
 
 
 def check_output():
@@ -15,14 +31,14 @@ def check_output():
         if '\n' in output:
             line, output = output.split('\n', 1)
 
-            # FIXME not sure where it goes, program gets here,
-            # but nothing is printed
+            # Python (Win) / tmux (Cygwin): gets printed with quite a delay
             print(line)
 
 
 def send_key(e):
     if e.event_type == 'down':
-        ser.write([e.scan_code % LEDS])
+        value = get_ino_value(e.name, e.scan_code % LEDS)
+        ser.write([value])
 
 
 def main():
